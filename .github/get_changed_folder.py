038b3b02cbac6a7d062ec508bb1564b06c2a5dcb
Eugene Roslyakov <ei.roslyakov@gmail.com>
2022-09-06 10:12:02 +0300

    some fixs

--- .github/get_changed_folder.py
+++ .github/get_changed_folder.py
@@ -6,7 +6,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-f", "--folders", nargs="+", default=[]),
-    parser.add_argument("-d", "--lookup-dirs", nargs="+", default=[
+    parser.add_argument("-d", "--lookup-dirs", nargs="+", required=False, default=[
         "dev",
         "stage",
         "prod",
